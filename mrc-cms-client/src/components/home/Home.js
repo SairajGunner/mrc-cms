e2f370eaa7d7a38048c5540bc08bc1855f23b61a
Sairaj Udayshankar <sairaj9396@gmail.com>
2023-07-30 19:58:54 -0400

    Made changes to the Engagement Component

--- mrc-cms-client/src/components/home/Home.js
+++ mrc-cms-client/src/components/home/Home.js
@@ -3,26 +3,32 @@ import "./Home.scss";
 import Accordion from "../shared/accordion/Accordion";
 import NoteBox from "../shared/note-box/NoteBox";
 import { NotesAPI } from "../../services/notes-service.js";
+import { EngagementsAPI } from "../../services/engagements-service";
+import EngagementDisplay from "../shared/engagement-display/EngagementDisplay";
 
 export default class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      notes: []
+      notes: [],
+      engagements: []
     };
-    this.getNotesbyCustomerId();
+    this.getNotesByCustomerId();
+    this.getEngagementsByCustomerId();
   }
 
   componentDidUpdate(previousProps, previousState) {
     if (previousProps.selectedCustomer !== this.props.selectedCustomer) {
       this.setState({
-        notes: []
+        notes: [],
+        engagements: []
       });
-      this.getNotesbyCustomerId();
+      this.getNotesByCustomerId();
+      this.getEngagementsByCustomerId();
     }
   }
 
-  getNotesbyCustomerId = () => {
+  getNotesByCustomerId = () => {
     if (this.props.selectedCustomer) {
       NotesAPI.getNotesByCustomerId(this.props.selectedCustomer.id)
         .then((response) => {
@@ -30,7 +36,23 @@ export default class Home extends Component {
         })
         .then((data) => {
           this.setState({
-            notes: data
+            notes: data,
+            engagements: this.state.engagements
+          });
+        });
+    }
+  };
+
+  getEngagementsByCustomerId = () => {
+    if (this.props.selectedCustomer) {
+      EngagementsAPI.getEngagementsByCustomerId(this.props.selectedCustomer.id)
+        .then((respose) => {
+          return respose.json();
+        })
+        .then((data) => {
+          this.setState({
+            notes: this.state.notes,
+            engagements: data
           });
         });
     }
@@ -44,59 +66,70 @@ export default class Home extends Component {
             ? this.props.selectedCustomer.name
             : undefined}
         </h2>
-        <Accordion id="accordion-details" title="Details">
-          <div className="details-holder">
-            <table>
-              <tbody>
-                <tr>
-                  <td>Name:</td>
-                  <td>
-                    {this.props.selectedCustomer
-                      ? this.props.selectedCustomer.name
-                      : undefined}
-                  </td>
-                </tr>
-                <tr>
-                  <td>Organization:</td>
-                  <td>
-                    {this.props.selectedCustomer
-                      ? this.props.selectedCustomer.organization
-                      : undefined}
-                  </td>
-                </tr>
-                <tr>
-                  <td>Customer Since:</td>
-                  <td>
-                    {this.props.selectedCustomer
-                      ? this.props.selectedCustomer.dateOfAssociation
-                      : undefined}
-                  </td>
-                </tr>
-                <tr>
-                  <td>Area of Interest:</td>
-                  <td>
-                    {this.props.selectedCustomer
-                      ? this.props.selectedCustomer.areaOfInterest
-                      : undefined}
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </Accordion>
-        <Accordion
-          id="accordion-engagements"
-          title="Previous Engagements"
-        ></Accordion>
-        <Accordion id="accordion-notes" title="Notes">
-          {this.state.notes.map((note, index) => {
-            return <NoteBox key={index} note={note}></NoteBox>;
-          })}
-
-          <div className="home-add-note-button-container">
-            <button id="add-note">Add Note</button>
-          </div>
-        </Accordion>
+        <div id="accordion-details-container">
+          <Accordion id="accordion-details" title="Details">
+            <div className="details-holder">
+              <table>
+                <tbody>
+                  <tr>
+                    <td>Name:</td>
+                    <td>
+                      {this.props.selectedCustomer
+                        ? this.props.selectedCustomer.name
+                        : undefined}
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Organization:</td>
+                    <td>
+                      {this.props.selectedCustomer
+                        ? this.props.selectedCustomer.organization
+                        : undefined}
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Customer Since:</td>
+                    <td>
+                      {this.props.selectedCustomer
+                        ? this.props.selectedCustomer.dateOfAssociation
+                        : undefined}
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Area of Interest:</td>
+                    <td>
+                      {this.props.selectedCustomer
+                        ? this.props.selectedCustomer.areaOfInterest
+                        : undefined}
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </Accordion>
+        </div>
+        <div id="accordion-engagements-container">
+          <Accordion id="accordion-engagements" title="Previous Engagements">
+            {this.state.engagements && this.state.engagements.length > 0 && (
+              <EngagementDisplay
+                engagements={this.state.engagements}
+              ></EngagementDisplay>
+            )}
+            <div className="home-add-button-container">
+              <button className="btn-add">Add Engagement</button>
+            </div>
+          </Accordion>
+        </div>
+        <div id="accordion-notes-container">
+          <Accordion id="accordion-notes" title="Notes">
+            {this.state.notes.map((note, index) => {
+              return <NoteBox key={index} note={note}></NoteBox>;
+            })}
+            <div className="home-add-button-container">
+              <button className="btn-add">Add Note</button>
+            </div>
+          </Accordion>
+        </div>
       </div>
     );
   }
--- mrc-cms-client/src/components/shared/engagement-display/EngagementDisplay.js
+++ mrc-cms-client/src/components/shared/engagement-display/EngagementDisplay.js
@@ -2,24 +2,101 @@ import { Component } from "react";
 import "./EngagementDisplay.scss";
 
 export default class EngagementDisplay extends Component {
-    render() {
-        return (
-            <div className="engagement-display-container">
-                <table>
-                    <thead>
-                        <th>
-                            Engagement Name
-                        </th>
-                        <th>
-                            Period
-                        </th>
-                        <th>
-                            Hours / Week
-                        </th>
-                    </thead>
-                    <tbody></tbody>
-                </table>
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedEngagement: undefined
+    };
+  }
+
+  weeksBetween = (date1, date2) => {
+    return isNaN(
+      Math.round(
+        (new Date(date2) - new Date(date1)) / (7 * 24 * 60 * 60 * 1000)
+      )
+    )
+      ? "-"
+      : Math.round(
+          (new Date(date2) - new Date(date1)) / (7 * 24 * 60 * 60 * 1000)
+        );
+  };
+
+  render() {
+    return (
+      <div id="engagement-display-container">
+        <div id="engagement-display-table">
+          <table>
+            <thead>
+              <tr>
+                <th>Engagement Name</th>
+                <th>Period</th>
+                <th>Hours / Week</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.props.engagements.map((engagement) => {
+                return (
+                  <tr
+                    key={engagement.id}
+                    style={{
+                      color:
+                        this.state.selectedEngagement == engagement
+                          ? "#143670"
+                          : "black"
+                    }}
+                    className="selected-engagement"
+                    onClick={() =>
+                      this.setState({ selectedEngagement: engagement })
+                    }
+                  >
+                    <td>{engagement.name}</td>
+                    <td>
+                      {this.weeksBetween(
+                        engagement.startDate,
+                        engagement.endDate
+                      )}{" "}
+                      weeks
+                    </td>
+                    <td>{engagement.hoursOfWork}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+        {this.state.selectedEngagement && (
+          <div id="engagement-box-container">
+            <div id="engagement-box-header">
+              {this.state.selectedEngagement &&
+                this.state.selectedEngagement.name}
             </div>
-        )
-    }
+            <div id="engagement-box-content">
+              <div id="engagement-box-content-dates">
+                <div id="engagement-box-content-dates-startDate">
+                  Start Date:{" "}
+                  {this.state.selectedEngagement &&
+                    this.state.selectedEngagement.startDate}
+                </div>
+                <div id="engagement-box-content-dates-endDate">
+                  End Date:{" "}
+                  {this.state.selectedEngagement &&
+                    this.state.selectedEngagement.endDate}
+                </div>
+              </div>
+              <div id="engagement-box-content-area-of-work">
+                Area of Work:{" "}
+                {this.state.selectedEngagement &&
+                  this.state.selectedEngagement.areaOfWork}
+              </div>
+              <div id="engagement-box-content-remarks">
+                Remarks:{" "}
+                {this.state.selectedEngagement &&
+                  this.state.selectedEngagement.remarks}
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
